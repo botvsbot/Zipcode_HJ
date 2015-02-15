8512fc1334c745cf294c88b73885f18eb4b3fab4
Rajaganesh Ganesh <g.rajaganesh@gmail.com>
2015-02-15 12:22:47 -0800

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class Address(Form):
-    addressa = StringField('addressa', validators=[DataRequired()])
-    addressb = StringField('addressb', validators=[DataRequired()])
